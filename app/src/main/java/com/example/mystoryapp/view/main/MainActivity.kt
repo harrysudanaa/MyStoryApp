a5d588f113a3ac27601c91593f931fb0c4af3f33
harrysudanaa <komangharry02@gmail.com>
2024-04-25 06:26:30 +0800

    Add logout button and functionality

--- app/src/main/java/com/example/mystoryapp/view/main/MainActivity.kt
+++ app/src/main/java/com/example/mystoryapp/view/main/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.mystoryapp.view.main
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mystoryapp.R
 import com.example.mystoryapp.data.remote.response.ListStoryItem
 import com.example.mystoryapp.data.remote.response.StoryResponse
 import com.example.mystoryapp.databinding.ActivityMainBinding
@@ -39,6 +43,33 @@ class MainActivity : AppCompatActivity() {
         setupAction()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.logout -> {
+                AlertDialog.Builder(this).apply {
+                    setTitle("Logout")
+                    setMessage("Are you sure to logout?")
+                    setPositiveButton("Yes") { _, _ ->
+                        mainViewModel.logout()
+                    }
+                    setNegativeButton("No") { _, _ ->
+                        return@setNegativeButton
+                    }
+                    create()
+                    show()
+                }
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     private fun setupAction() {
         binding.fabAddStory.setOnClickListener {
             val intent = Intent(this, AddStoryActivity::class.java)
@@ -53,7 +84,7 @@ class MainActivity : AppCompatActivity() {
         } else {
             window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         }
-        supportActionBar?.hide()
+        setSupportActionBar(binding.toolbarMain.root)
 
         binding.rvListStory.layoutManager = LinearLayoutManager(this)
         binding.rvListStory.adapter = storyAdapter
